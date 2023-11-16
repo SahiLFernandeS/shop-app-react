fbfa7d6185a97ea89e93cccbbd5701af0ad0fe57
Sahil Fernandes <fernandessahil42@gmail.com>
2023-11-16 19:55:06 +0530

    project initialize

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from './Components/Dashboard/Dashboard.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
